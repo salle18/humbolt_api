180549b2b182fb521a2cb422b1220a5b758e93f3
salle18 <salle18@gmail.com>
2016-02-29 19:19:56 +0100

    AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,7 @@ class AuthController extends Controller
     private $client;
 
     private $moodle_uri = "https://moodle.elab.fon.bg.ac.rs/login/token.php";
+
     /**
      * Create a new controller instance.
      *
@@ -23,14 +24,30 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
+        $name = $request->input("name");
+        $password = $request->input("password");
         $response = $this->client->request("POST", $this->moodle_uri, [
             "verify" => false,
             "form_params" => [
-                "username" => $request->input("name"),
-                "password" => $request->input("password"),
+                "username" => $name,
+                "password" => $password,
                 "service" => "moodle_mobile_app"
             ]
         ]);
-        return $response->getBody();
+        $body = json_decode($response->getBody());
+        if ($body->token) {
+            if (is_null($user = User::where("name", $name))) {
+                $user = $this->create_user($name);
+                $token = JWTAuth::fromUser($user);
+                return response()->json(["token" => $token]);
+            }
+        } else {
+            return $body;
+        }
+    }
+
+    protected function create_user($name)
+    {
+        return User::create(["name" => $name]);
     }
 }
