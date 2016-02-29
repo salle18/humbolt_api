b56d4ae41b150b872302639577fa605e0c4e99e2
aleksandar.arsenovic <salle18@gmail.com>
2016-02-29 15:43:27 +0100

    dodate zavisne biblioteke

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -49,18 +49,3 @@ Route::group(['prefix' => 'api/v1'], function () {
     });
 
 });
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
-Route::group(['middleware' => ['web']], function () {
-    //
-});
