2a42d0918838bf4169714b10e4b47b23184ee15b
aleksandar.arsenovic <salle18@gmail.com>
2016-03-01 14:39:56 +0100

    csmp i gpss simulacije razdvojene u svoje modele za buducnost

--- app/CsmpSimulation.php
+++ app/CsmpSimulation.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Simulation extends Model
+class CsmpSimulation extends Model
 {
     protected $fillable = [
         'description', 'data'
---
+++ app/GpssSimulation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GpssSimulation extends Model
+{
+    protected $fillable = [
+        'description', 'data'
+    ];
+}
