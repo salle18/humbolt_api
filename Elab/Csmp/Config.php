37787b2440e95b98ad8991f34f725f368bf73603
aleksandar.arsenovic <salle18@gmail.com>
2016-03-01 13:25:01 +0100

    getMethod i getBlock dodati ovde ali ih treba izmestiti

--- Elab/Csmp/Config.php
+++ Elab/Csmp/Config.php
@@ -2,22 +2,53 @@
 
 namespace Elab\Csmp;
 
+use Elab\Csmp\Exceptions\BlockNotFoundException;
+use Elab\Csmp\Exceptions\MethodNotFoundException;
+
 class Config
 {
+
+    private $block_classes;
+
+    private $method_classes;
+
+    public function __construct()
+    {
+        $this->block_classes = require "config/blocks.php";
+        $this->method_classes = require "config/methods.php";
+    }
+
     public function blocks()
     {
-        $block_classes = require "config/blocks.php";
         return array_map(function ($block_class) {
             return new $block_class();
-        }, $block_classes);
+        }, $this->block_classes);
     }
 
     public function methods()
     {
-        $method_classes = require "config/methods.php";
         return array_map(function ($method_class) {
             return new $method_class();
-        }, $method_classes);
+        }, $this->method_classes);
+    }
+
+    public function getMethod($method, $simulation = null)
+    {
+        $key = array_search($method, $this->method_classes);
+        if ($key !== false) {
+            return new $this->method_classes[$key]($simulation);
+        }
+        throw new MethodNotFoundException("Nije pronađena metoda integracije.");
+    }
+
+    public function getBlock($block)
+    {
+        $key = array_search($block, $this->block_classes);
+        if ($key !== false) {
+            return new $this->block_classes[$key]();
+        }
+        throw new BlockNotFoundException("Nije pronađena implementacija datog bloka.");
+
     }
 
 }
