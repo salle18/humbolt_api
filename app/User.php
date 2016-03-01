165b8851ebafcef31a1bc96589a104d243bf7a76
aleksandar.arsenovic <salle18@gmail.com>
2016-03-01 13:24:39 +0100

    User mora da bude authenticatable

--- app/User.php
+++ app/User.php
@@ -2,10 +2,14 @@
 
 namespace App;
 
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class User extends Model implements AuthenticatableContract
 {
+    use Authenticatable;
+
     /**
      * The attributes that are mass assignable.
      *
