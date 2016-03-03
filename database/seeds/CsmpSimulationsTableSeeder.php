52da07adb0d7ceff742669b3b3ffff769f0d63a3
aleksandar.arsenovic <salle18@gmail.com>
2016-03-03 11:59:26 +0100

    db seeder dodat

---
+++ database/seeds/CsmpSimulationsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CsmpSimulationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $simulations = $this->read_simulations();
+        foreach ($simulations as $simulation) {
+            DB::table('csmp_simulations')->insert([
+                'user_id' => 1,
+                'description' => $simulation['description'],
+                'data' => json_encode($simulation)
+            ]);
+        }
+    }
+
+    private function read_simulations()
+    {
+        $simulations = [];
+        $files = File::files(__DIR__ . '/csmp_simulations');
+        natsort($files);
+        foreach ($files as $filename) {
+            $file = File::get($filename);
+            $simulations[] = json_decode($file, true);
+        }
+        return $simulations;
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('users')->truncate();
+        DB::table('csmp_simulations')->truncate();
+        DB::table('gpss_simulations')->truncate();
+        $this->call(UsersTableSeeder::class);
+        $this->call(CsmpSimulationsTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
