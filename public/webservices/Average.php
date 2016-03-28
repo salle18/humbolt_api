5703ce0989339241fcec80dbeb9bbb9a1c4b1694
aleksandar.arsenovic <salle18@gmail.com>
2016-03-28 12:50:24 +0200

    novi klijent

--- public/webservices/Average.php
+++ public/webservices/Average.php
@@ -7,7 +7,7 @@ $connections = array_map('intval', $_GET['connections']);
 $filtered = array_intersect_key($inputs, array_flip($connections));
 
 if (count($filtered) > 0) {
-    echo array_sum($filtered) / count($filtered);
+	echo array_sum($filtered) / count($filtered);
 } else {
-    echo 0;
+	echo 0;
 }
