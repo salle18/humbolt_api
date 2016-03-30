3ebad676306d19f2112a1f42d62a9091ae70997d
aleksandar.arsenovic <salle18@gmail.com>
2016-03-30 13:35:24 +0200

    male ispravke lepsa sintaksa

--- app/Http/Controllers/GpssController.php
+++ app/Http/Controllers/GpssController.php
@@ -23,25 +23,27 @@ class GpssController extends Controller
     public function index()
     {
         $user = Auth::user();
-        return response()->json($user->gpsssimulations()->get(['id', 'description', 'created_at']));
+        $simulations = $user->gpsssimulations()->get(['id', 'description', 'created_at']);
+        return response()->json($simulations);
     }
 
     public function show($id)
     {
         $user = Auth::user();
-        $data = $user->gpsssimulations()->where('id', $id)->first(['data']);
-        return response($data['data'], Response::HTTP_OK, ['Content-Type' => 'application/json']);
+        $simulation = $user->gpsssimulations()->where('id', $id)->first(['data']);
+        return response()->json($simulation['data']);
     }
 
     public function create(Request $request)
     {
         $user = Auth::user();
         $data = $request->all();
-        $simulation = GpssSimulation::create([
-            'user_id' => $user->id,
+        $simulation = new GpssSimulation([
             'description' => $data['description'],
             'data' => json_encode($data)
         ]);
+        $simulation->user_id = $user->id;
+        $simulation->save();
         return response()->json(['_id' => $simulation->id]);
     }
 
