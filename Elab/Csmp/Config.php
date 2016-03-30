74d931d0c4dfac0146705988b0905c8641690e5c
salle18 <salle18@gmail.com>
2016-03-30 23:06:45 +0200

    config nam je neophodan za normalno ucitavanje dostupnih blokova i methoda

--- Elab/Csmp/Config.php
+++ Elab/Csmp/Config.php
@@ -2,32 +2,14 @@
 
 namespace Elab\Csmp;
 
-use Elab\Csmp\Exceptions\BlockNotFoundException;
-use Elab\Csmp\Exceptions\MethodNotFoundException;
-
 /**
  * Class Config
- * Klasa koja se koristi za učitavanje dostupnih blokova i metoda integracije kao i za instanciranje blokova i metoda iz naziva klase.
+ * Klasa koja se koristi za učitavanje dostupnih blokova i metoda integracije.
  *
  * @package Elab\Csmp
  */
 class Config
 {
-
-    /**
-     * @var string[] Niz dostupnih blokova.
-     */
-    private $block_classes;
-    /**
-     * @var string[] Niz dostupnih metoda integracije.
-     */
-    private $method_classes;
-
-    public function __construct()
-    {
-        $this->block_classes = require "config/blocks.php";
-        $this->method_classes = require "config/methods.php";
-    }
 
     /**
      * Vraća niz dostupnih methoda integracije.
@@ -36,9 +18,9 @@ class Config
      */
     public function methods()
     {
-        return array_map(function ($method_class) {
-            return new $method_class();
-        }, $this->method_classes);
+        return array_map(function ($qualifiedClassName) {
+            return new $qualifiedClassName();
+        }, require "config/methods.php");
     }
 
     /**
@@ -48,44 +30,9 @@ class Config
      */
     public function blocks()
     {
-        return array_map(function ($block_class) {
-            return new $block_class();
-        }, $this->block_classes);
-    }
-
-    /**
-     * Kreira instancu date metode po nazivu klase.
-     *
-     * @param string $method
-     * @param Simulation $simulation
-     * @return IntegrationMethod
-     * @throws MethodNotFoundException
-     */
-    public function getMethod($method, $simulation = null)
-    {
-        $method = "Elab\\Csmp\\Methods\\" . $method;
-        $key = array_search($method, $this->method_classes);
-        if ($key !== false) {
-            return new $this->method_classes[$key]($simulation);
-        }
-        throw new MethodNotFoundException("Nije pronađena metoda integracije.");
-    }
-
-    /**
-     * Kreira instancu datog bloka po nazivu zadate klase.
-     *
-     * @param string $block
-     * @return Block
-     * @throws BlockNotFoundException
-     */
-    public function getBlock($block)
-    {
-        $block = "Elab\\Csmp\\Blocks\\" . $block;
-        $key = array_search($block, $this->block_classes);
-        if ($key !== false) {
-            return new $this->block_classes[$key]();
-        }
-        throw new BlockNotFoundException("Nije pronađena implementacija datog bloka.");
+        return array_map(function ($qualifiedClassName) {
+            return new $qualifiedClassName();
+        }, require "config/blocks.php");
     }
 
 }
