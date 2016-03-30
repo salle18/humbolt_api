603113ffe931afa4b07d5d8c017e4312c0f6784e
aleksandar.arsenovic <salle18@gmail.com>
2016-03-30 13:01:47 +0200

    U klasu integrator ugradjene medju vrednosti i inkrementi

--- Elab/Csmp/Blocks/Integrator.php
+++ Elab/Csmp/Blocks/Integrator.php
@@ -19,23 +19,79 @@ class Integrator extends Block
     protected $numberOfParams = 3;
     protected $maxNumberOfInputs = 3;
 
+    /**
+     * @var array Inkrementi integratora.
+     */
+    private $increments;
+
+    /**
+     * Inicijalizuje integrator.
+     */
     public function init()
     {
         $this->result = $this->params[0];
+        $this->intermediate = 0;
+        $this->memory = 0;
+        $this->increments = [0];
     }
 
+    /**
+     * Vraća među vrednost.
+     *
+     * @return float
+     */
     public function getIntermediate()
     {
         return $this->intermediate;
     }
 
-    public function setIntermediate($intermediate)
-    {
-        $this->intermediate = $intermediate;
-    }
-
+    /**
+     * Postavlja među vrednost.
+     */
     public function calculateResult()
     {
         $this->intermediate = $this->inputs[0]->result + $this->params[1] * $this->inputs[1]->result + $this->params[2] * $this->inputs[2]->result;
     }
+
+    /**
+     * Vraća zapamćenu vrednost rezultata.
+     *
+     * @return float
+     */
+    public function getPrevious()
+    {
+        return $this->memory;
+    }
+
+    /**
+     * Pamti trenutnu vrednost rezulata.
+     */
+    public function setPrevious()
+    {
+        $this->memory = $this->result;
+    }
+
+    /**
+     * Vraća vrednost inkrementa za zadati indeks.
+     *
+     * @param integer $index
+     * @return float
+     */
+    public function getIncrement($index)
+    {
+        return $this->increments[$index];
+    }
+
+    /**
+     * Postavlja vrednost inkrementa za zadati indeks.
+     *
+     * @param integer $index
+     * @param float $value
+     */
+    public function setIncrement($index, $value)
+    {
+        $this->increments[$index] = $value;
+    }
+
+
 }
